c216805c82735c8bb91f434a31497548e84c3ea6
Bari1192 <barabas.m4rk@gmail.com>
2024-07-23 17:22:50 +0200

    Adatbázis fejlesztési csomag [nagy] - DataBase Update Package [Huge]
    A fejlesztési / kódolási csomag részletei:
    FIXÁLVA lett:
    Autok táblában <> kW + LE (teljesitmeny oszlop) összetett mezők szétválasztásra kerültek, ugyanis a LE-nek az adatbázisban és a későbbi felhasználásban nincs releváns értéke. Ebből kifolyólag csak a kW maradt meg, mely a 'teljesitmeny' oszlopban egy cellában már csak egy adatot jelenít meg.
    - A felhasznált statisztika hozzáadásra került 'forrás(1,2,3...)' néven az adott tábla mező értékéhez a 'Kivitelezési tervek.docx' fájlban.
    Személy táblában <> Jogosítvány érvényesség és lejárat dátum alapján felvételre került.
    Személy táblában <> A jelszó második és utolsó számjegyét a személy táblában lévő 'felh_jelszo' -ból fogja a rendszer kivenni.
    <> Brainstorming fájlban az előfizetési kategóriák példa értékei bekerültek.
    <> Ékezetek javítva az adattáblákban.
    
    [MEMÓRIA FELSZABADÍTÁSOK]
    - A 'kat_modell_nev' mérete 10 karakter hosszúságra maximalizálva - A 'Kangoo Z.E.' méretére.
    - Az egyes autótípusoknak/személyigazolványok/jogosítványok/vezeték és keresztnév stb. adatoknak a hossza SQL kódban csökkentve lett statisztikai alapon.
    - A teljesítmény most már nem 'INTEGER'-ként, hanem TINYINT + CHECK-el ellenőrzi a megadott 5 típus teljesítményének helyes felvitelét.
    - Végsebesség értéke TINYINT-re módosítva, mivel egyik autó sem éri el a 255 km/h végsebességet.
    - Felhasználónév hossza 20 karakterre lett szűkítve.
    - Jogosítvány száma 15 karakterben maximalizálva a külföldi felhasználókat is figyelembe véve.
    - Vezeték és keresztnév 35-35 karakterben maximalizálva. Ennél hosszabb nagyon ritka esetekben fordul elő.
    - A személyigazolvány 15 karakterben maximalizálva a külföldi felhasználók hibaelkerülése végett.
    - Telefonszám az ország/hívószám és whitespace-eket tekintve egységesen 19 karakterben maximalizáltam.
    - E-mail cím 50 maximálisan karakter hosszú lehet.
    
    NOT FIXED:
    - Adott autóhoz eltérő teljesítményt be lehet írni (pl: e-up!-hoz 50 kW-ot).
    - A felh_jelszo a személy táblában nincsen titkosítva, így hozzáférhető marad(t).
    -     A jogositvany_ervenyesseg nem lehet korábbi a mai dátumnál + 1 év.
    -     A jogositvany_lejarata nem lehet korábbi a (mai dátumnál és az érvényesség kezdeténél)
    -     A születési dátum nem lehet korábbi a mai dátumnál.
    -     A születési dátum - aktuális dátum alapján minimum 18 évesnek kell lennie.
    -     A bérlés kezdeti időpontja lehet üres mező, de:
    -     Amennyiben van ’berles_kezd’-hez dátum megadva, akkor folyamatban lévő bérlésről beszélünk, így a ’berles_veg’ rész csak akkor tölthető ki, ha a berles kezdete sem üres mező.
    -     A bérlés vége nem mutathat túl az aktuális napi dátumnál! (yyyy.mm.dd)

--- Adatbázis Szerkezeti felépítése/PowerAndGo.sql
+++ Adatbázis Szerkezeti felépítése/PowerAndGo.sql
@@ -2,9 +2,93 @@
 CREATE DATABASE PowerAndGo
 CHARACTER SET utf8
 COLLATE utf8_hungarian_ci;
-USE PowerGoDB;
+USE PowerAndGo;
 
-CREATE TABLE Berautok (
-    ber_auto_id INT PRIMARY KEY,
-    tipus VARCHAR(50)
+-- Adatbázis kódleírása:
+-- | Balról jobbra | Kategóriák | Felszereltség | Autok | Bérlés | Felhasználók | Személy
+-- Rendszám (AA-CO-123 | ABC-123) elfogadható.
+-- CHECK-kel biztosítom, hogy az oszlopban csak a meghatározott feltételeknek [CSAK SZÁMOKNÁL] megfelelően szerepelhessenek adatok. Helytelen adatot 'ellenőrzi'.
+-- PERSISTENT-tel biztosítom, hogy nem kell újra és újra lekérdezni és kiszámítania az értéket, hanem letárolódik az adatbázisban. Gyorsabb lesz az adatlekérdezés. Aktuálisan frissül is.
+-- Memória sprólás végett INT helyett TINYINT (0-255) tárolunk olyan adatokat, amik ezen a tartományon biztosan nem esnek kívül. (kor, végseb., teljesítmény)
+-- Bérlések táblában ahol a kezd. dátuma meg van adva, de a bérlés végének dátuma üres, ott folytatólagos a bérlés. [Külön szűrhető]
+
+CREATE TABLE Kategoriak (
+   kat_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+   kat_besorolas ENUM('0', '1', '2', '3', '4'),
+   kat_modell_nev VARCHAR(10),
+   
+   autok_id_FK INT,
+   FOREIGN KEY (autok_id_FK)
+   REFERENCES Autok(autok_id)
+
+);
+
+CREATE TABLE Autok (
+    autok_id INT AUTO_INCREMENT PRIMARY KEY,
+    gyartmany ENUM('Skoda', 'VW', 'KIA', 'Opel', 'Renault'),
+    tipus VARCHAR(62) NOT NULL,
+    rendszam VARCHAR(9) NOT NULL,
+    teljesitmeny TINYINT CHECK(teljesitmeny IN (18,36,65,50,45)),
+    gyorsulas Decimal(3,1) NOT NULL,
+    vegsebesseg TINYINT NOT NULL,
+    gumimeret VARCHAR(15) NOT NULL,
+    hatotav INT CHECK (hatotav BETWEEN 150 AND 350) NOT NULL, -- CSEKKOLNI!
+    gyartasi_ev ENUM('2020', '2021', '2022', '2023')
+);
+
+CREATE TABLE Felszereltseg (
+    felszereltseg_id INT AUTO_INCREMENT PRIMARY KEY,
+    tolatokamera ENUM('igen', 'nem'),
+    m_kormany ENUM('igen', 'nem'),
+    savtarto ENUM('igen', 'nem'),
+    tempomat ENUM('igen', 'nem'),
+    
+    auto_id_FK INT,
+    FOREIGN KEY (auto_id_FK)
+    REFERENCES Autok(autok_id)
+);
+
+CREATE TABLE Berles (
+    berles_azon INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    berles_kezd DATE, -- 'NOT NULL' nem kerül ide, mert nem minden regisztrált felhasználó bérel egyszerre autót.
+    berles_veg DATE,
+    
+    kat_FK INT,
+    FOREIGN KEY (kat_FK)
+    REFERENCES Kategoriak(kat_id),
+    
+    felh_nev_FK INT NOT NULL,
+    FOREIGN KEY (felh_nev_FK)
+    REFERENCES Felhasznalok(felh_id),
+    
+    rendszam_FK VARCHAR(9),
+    FOREIGN KEY (rendszam_FK)
+    REFERENCES Autok(rendszam)
+);
+
+CREATE TABLE Felhasznalok (
+    felh_id INT AUTO_INCREMENT PRIMARY KEY,
+    felh_nev VARCHAR(20) UNIQUE NOT NULL, -- Egy f.nevből CSAK egy lehet az oszlopban!
+    elofiz_kat ENUM('0', '1', '2', '3'),
+    jogositvany_szama_FK  VARCHAR(15) NOT NULL,
+    jelszo_masodik_utolso_szamjegye CHAR(2),
+
+    FOREIGN KEY (jogositvany_szama_FK)
+    REFERENCES Szemely(jogositvany_szama)
+);
+
+CREATE TABLE Szemely (
+    jogositvany_szama VARCHAR(15) PRIMARY KEY,
+    jogositvany_ervenyesseg DATE NOT NULL,
+    jogositvany_lejarata DATE NOT NULL,
+    V_nev VARCHAR(35) NOT NULL,
+    K_nev VARCHAR(35) NOT NULL,
+    Szig_szam VARCHAR(15) NOT NULL,
+    felh_jelszo VARCHAR(10) NOT NULL, -- Hash-elt jelszó (python bcrypt-tel kéne)
+    szul_datum DATE NOT NULL,
+    telefon VARCHAR(19) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+
+    kor AS (YEAR(CURDATE()) - YEAR(szul_datum)) PERSISTENT,
+    jogosítvány_ideje AS (YEAR(CURDATE()) - YEAR(jogositvany_ervenyesseg)) PERSISTENT
 );
