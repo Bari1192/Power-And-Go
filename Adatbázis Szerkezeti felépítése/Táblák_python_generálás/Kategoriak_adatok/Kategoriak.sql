c8e7980b817140ec4e2bea330d3b23c578d55400
Bari1192 <barabas.m4rk@gmail.com>
2024-07-24 15:46:06 +0200

    Kategóriák adatainak generálása Pythonban - SQL fájl készítése.
    Lekódoltam a 'Kategoriák' tábla adatsorait Pythonban, futtattam run és debug tesztekkel, és legeneráltam az SQL fájlt. A kód a Python fájl könyvtárába menti az elkészült SQL fájlt.
    
    - Készítettem egy `kategoria_generalas` függvényt, amely generálja a 'Kategoriák' tábla adatait és SQL beszúrási utasításokat.
    - A generált SQL utasításokat úgy állítottam össze, hogy az utolsó sor pontosvesszővel záruljon, míg az összes többi sor végén vessző van.
    - Az aktuális Python fájl könyvtárába mentem az elkészült SQL fájlt az `os` modul segítségével.
    - A kód a `mentes_helye` változóban meghatározott útvonalra menti a fájlt.
    - Run & debug tesztekkel ellenőriztem a kód helyes működését, biztosítva, hogy a fájl megfelelően generálódik és a megfelelő helyre kerül.

---
+++ Adatbázis Szerkezeti felépítése/Táblák_python_generálás/Kategoriak_adatok/Kategoriak.sql
@@ -0,0 +1,7 @@
+INSERT INTO Kategoriak (kat_id, kategoria_besorolas, kat_modell_nev) VALUES
+(1, 0, 'e-Up!'),
+(2, 1, 'e-Up!'),
+(3, 1, 'Citigo'),
+(4, 2, 'Niro'),
+(5, 3, 'Vivaro-e'),
+(6, 4, 'Kangoo Z.E.');
