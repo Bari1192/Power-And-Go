495d82c27d415616a3dc08b8add1eb6c2f736931
bari1192 <barabas.m4rk@gmail.com>
2024-11-30 15:54:52 +0000

    lapozó fül hozzáadása a táblázat és a visszaigazoló sablon e-mailhez, tizesével csoportosítva. Az e-mail Tailwind-del szerkesztve, megjelenése módosítva.

--- fullstack_0.2/frontend/src/pages/rents/renthistory.vue
+++ fullstack_0.2/frontend/src/pages/rents/renthistory.vue
@@ -1,40 +1,81 @@
 <template>
     <BaseLayout>
+        <!-- LAPOZÓ -->
+        <div class="pagination text-center my-5">
+            <button v-for="page in totalPages" :key="page" @click="changePage(page)"
+                :class="{ 'bg-sky-700 text-white': page === currentPage, 'bg-sky-500 text-black': page !== currentPage }"
+                class="px-3 py-2 m-1 border rounded">
+                {{ page }}
+            </button>
+        </div>
 
-        <div class="container mx-auto my-10" v-for="rent in rents" :key="rent.lezart_berles_id">
+        <!-- VISSZAIGAZOLÓ E-MAIL TELJES SZERKEZETE -->
+        <div class="container mx-auto my-10" v-for="rent in paginatedRents" :key="rent.lezart_berles_id">
             <div class="col w-3/4 m-auto">
-                <img src="@assets/img/BaseEmail/pelda_logo.png" alt="">
+                <img src="@assets/img/BaseEmail/pelda_logo.png" class="opacity-80">
 
             </div>
-            <div class="col text-white col w-3/4  mx-auto">
-                <p class="my-3"><b>Kedves {{ rent.szemely_knev }}!</b></p>
-                <p>Köszönjük, hogy a <b>PowerAndGo</b> e-carsharinget választottad!</p>
-                <p> A(z) <b>{{ rent.auto_rendszam }}</b> rendszámú PowerAndGo bérlését <b>{{ rent.berles_kezdete }}</b>
-                    -kor kezdted
-                    és
-                    <b>{{ rent.berles_vege }}</b>
-                    -kor
-                    fejezted be.
-                </p>
-                <p>A bérlésed során 0h 00' hosszabbítás volt a foglalásodon,<b>{{ rent.berles_percek }}</b> vezettél, ami
-                    alatt
-                    {{
-                        rent.megtett_tavolsag }}
-                    km-t
-                    tettél meg, és a vezetés mellett 0h 46'-et parkoltál. A bérléshez felhasznált bónusz percek: 0h
-                    00'.
-                    A bérléshez az alábbi kuponokat váltottad be:</p>
+            <div class="col text-white col w-3/4  mx-auto text-justify">
+                <p class="my-3 text-xl text-lime-400 font-bold">Kedves {{ rent.szemely_knev }}!</p>
+                <p class="my-3 font-bold ">Köszönjük, hogy a PowerAndGo e-carsharinget választottad!</p>
 
-                <p>A bérlésed díját, 1,833 Ft-ot, hamarosan kiszámlázzuk neked. A részletes díjtételeket és az autó
-                    értékelése során készített képeidet tartalmazó bérlés összesítő dokumentumot innen tudod
-                    letölteni.
-                </p>
+                <!-- "Keret" kezdete -->
+                <div class=" border-solid border border-lime-600 p-6 m-2">
+                    <p class="my-3"> A(z) <b>{{ rent.auto_rendszam }}</b> rendszámú PowerAndGo bérlését <b>{{
+                        rent.berles_kezdete }}</b>
+                        -kor kezdted
+                        és
+                        <b>{{ rent.berles_vege }}</b>
+                        -kor
+                        fejezted be.
+                    </p>
+                    <p class="my-3">A bérlésed során 0h 00' hosszabbítás volt a foglalásodon, <b>{{ rent.berles_percek
+                            }}</b> percet vezettél,
+                        ami
+                        alatt
+                        <b> {{
+                            rent.megtett_tavolsag }}
+                            km-t</b>
+                        tettél meg, és a vezetés mellett ((0h 46'))-et parkoltál. A bérléshez felhasznált bónusz percek:
+                        0h
+                        00'.
+                        A bérléshez az alábbi kuponokat váltottad be:
+                    </p>
 
-                <p> Bérlésed alatt 16 kWh-t töltöttél az autóba más szolgáltató töltőoszlopán, ezért 4,200 Ft értékű
-                    PowerAndGo csomagot szereztél. A töltésért járó csomagokról bővebben itt olvashatsz.</p>
+                    <p>A bérlésed díját, ((1,833)) Ft-ot, hamarosan kiszámlázzuk neked. A részletes díjtételeket és az
+                        autó
+                        értékelése során készített képeidet tartalmazó bérlés összesítő dokumentumot <u>innen</u> tudod
+                        letölteni.
+                    </p>
+                </div>
+                <!-- "Keret" vége -->
+
+                <!-- 2 OPCIÓ => Töltőtt -->
+                <p class="mt-10"> Bérlésed alatt 0 kWh-t töltöttél az autóba más szolgáltató töltőoszlopán, ezért ((4,200)) Ft értékű
+                    PowerAndGo csomagot szereztél. A töltésért járó csomagokról bővebben <router-link
+                        to="/ugyfelszolgalat" class="underline underline-offset-4 text-lime-500 ">itt</router-link>
+                    olvashatsz.</p>
+                <br><!-- 2 OPCIÓ => Nem töltött -->
+                <p>Bérlésed alatt 0 kWh-t töltöttél az autóba más szolgáltató töltőoszlopán, ezért 0 Ft értékű
+                    PowerAndGo csomagot szereztél. A töltésért járó csomagokról bővebben <router-link
+                        to="/ugyfelszolgalat" class="underline underline-offset-4 text-lime-500 ">itt</router-link>
+                    olvashatsz.</p>
+
+                <p class="my-3 font-bold"> Reméljük, hogy élvezted a PowerAndGo-s utadat!</p>
+
+                <p class="mb-10"> Ha hibát észleltél vagy ötleted, észrevételed van a szolgáltatásunkkal kapcsolatban, kérjük, jelezd
+                    nekünk az <router-link to="/ugyfelszolgalat"
+                        class="underline underline-offset-4 text-lime-500 italic">ugyfelszolgalat@powerandgo.com</router-link>
+                    email címen!</p>
+
+                <p>Köszönettel,</p>
+                <p class="font-bold">PowerAndGo csapata</p>
+
+                <p class="my-3 font-bold italic"> Élmények. Neked. Nekünk. Tisztán.</p>
             </div>
         </div>
 
+        <!-- TÁBLÁZAT A LEKÉRT ADATOKKAL -->
         <table class="mx-auto my-10 w-3/4 border-2 border-sky-300 border-collapse">
             <thead>
                 <tr class="text-white text-xl bg-sky-600">
@@ -49,8 +90,8 @@
                 </tr>
             </thead>
             <tbody class=" text-white text-lg">
-                <tr v-for="rent in rents" :key="rent.lezart_berles_id"
-                    class=" text-white cursor-pointer odd:hover:bg-sky-900 even:hover:bg-sky-800 odd:bg-sky-500  even:bg-sky-600">
+                <tr v-for="rent in paginatedRents" :key="rent.lezart_berles_id"
+                    class=" text-white cursor-pointer odd:hover:bg-sky-900 even:hover:bg-sky-800 odd:bg-sky-500 even:bg-sky-600">
                     <td class="text-center p-2">{{ rent.lezart_berles_id }}</td>
                     <td class="text-center p-2">{{ rent.auto_rendszam }}</td>
                     <td class="text-center p-2">{{ rent.berles_kezdete }}</td>
@@ -62,6 +103,16 @@
                 </tr>
             </tbody>
         </table>
+
+        <!-- Lapozó -->
+        <div class="pagination text-center my-5">
+            <button v-for="page in totalPages" :key="page" @click="changePage(page)"
+                :class="{ 'bg-sky-700 text-white': page === currentPage, 'bg-sky-500 text-black': page !== currentPage }"
+                class="px-3 py-2 m-1 border rounded">
+                {{ page }}
+            </button>
+        </div>
+
 
     </BaseLayout>
 </template>
@@ -78,8 +129,28 @@ export default {
     },
     data() {
         return {
-            rents: []
+            rents: [],
+            currentPage: 1,
+            itemsPerPage: 10,
         }
+    },
+    computed: {
+        // # Az aktuális oldal - adatok.
+        paginatedRents() {
+            const start = (this.currentPage - 1) * this.itemsPerPage;
+            const end = start + this.itemsPerPage;
+            return this.rents.slice(start, end);
+        },
+        // # Az összes oldalszám meghatározása.
+        totalPages() {
+            return Math.ceil(this.rents.length / this.itemsPerPage);
+        },
+    },
+    methods: {
+        //# Oldalak közötti váltás mechanika.
+        changePage(page) {
+            this.currentPage = page;
+        },
     },
     async mounted() {
         try {
@@ -90,5 +161,4 @@ export default {
         }
     }
 }
-
 </script>
