438f4ee3a5040c2edc0522a9c16a94f669a58440
bari1192 <barabas.m4rk@gmail.com>
2024-12-04 19:47:22 +0000

    A Sablon e-mailnek a generálási törzse egészében elkészült a frontendes megjelenítésre. Egyedül a bónuszpercek és azok felhasználása nincsen beleimplementálva a programba. Az ehhez szükséges Resource-t frissítettem, illetve finomhangoltam a megejelnését az adatádásnál.

--- fullstack_0.2/backend/app/Http/Resources/LezartBerlesResource.php
+++ fullstack_0.2/backend/app/Http/Resources/LezartBerlesResource.php
@@ -13,18 +13,27 @@ class LezartBerlesResource extends JsonResource
         $berles_kezdete = Carbon::createFromFormat('Y-m-d H:i:s', $this->berles_kezd_datum . ' ' . $this->berles_kezd_ido);
         $berles_vege = Carbon::createFromFormat('Y-m-d H:i:s', $this->berles_veg_datum . ' ' . $this->berles_veg_ido);
 
-        $berles_percek = $berles_kezdete->diffInMinutes($berles_vege); ### Időtartam percekben
+
+
+        if ($this->vezetesi_perc == 0 && $this->vezetesi_perc % 60 == 0) {
+            $parkolasHaVolt = sprintf('%d percet parkoltál', $this->parkolasi_perc);
+        } elseif (intdiv($this->parkolasi_perc, 60) == 0) {
+            $parkolasHaVolt = sprintf('%d percet parkoltál', $this->parkolasi_perc % 60);
+        } else {
+            $parkolasHaVolt = sprintf('%d óra és %d percet parkoltál', intdiv($this->parkolasi_perc, 60), $this->parkolasi_perc % 60);
+        }
         return [
             'lezart_berles_id' => $this->lezart_berles_id,
-            'megtett_tavolsag'=>$this->megtett_tavolsag,
+            'megtett_tavolsag' => $this->megtett_tavolsag,
             ### Az AUTOMATA e-mail küldéshez kellenek ezek:
             'auto_rendszam' => $this->auto->rendszam,
             'berles_kezdete' => $berles_kezdete->format('Y-m-d H:i'),
             'berles_vege' => $berles_vege->format('Y-m-d H:i'),
-            'berles_percek' => number_format($berles_percek, 0, '', ''),
-
+            'parkolasi_idotartam' => $parkolasHaVolt,
+            'vezetesi_idotartam' => sprintf('%d óra és %d percet vezettél', intdiv($this->vezetesi_perc, 60), $this->vezetesi_perc % 60),
+            'berles_osszeg' => number_format($this->berles_osszeg, 0, '', ' '),
             ### A FELHASZNALO e-mail és k_nev lekérése más táblákból.
-            'szemely_knev'=>$this->felhasznalo->szemely->k_nev,
+            'szemely_knev' => $this->felhasznalo->szemely->k_nev,
             'szemely_email' => $this->felhasznalo->szemely->email,
         ];
     }
--- fullstack_0.2/frontend/src/pages/rents/renthistory.vue
+++ fullstack_0.2/frontend/src/pages/rents/renthistory.vue
@@ -29,20 +29,19 @@
                         -kor
                         fejezted be.
                     </p>
-                    <p class="my-3">A bérlésed során 0h 00' hosszabbítás volt a foglalásodon, <b>{{ rent.berles_percek
-                            }}</b> percet vezettél,
-                        ami
-                        alatt
-                        <b> {{
-                            rent.megtett_tavolsag }}
-                            km-t</b>
-                        tettél meg, és a vezetés mellett ((0h 46'))-et parkoltál. A bérléshez felhasznált bónusz percek:
+                    <p class="my-3">A bérlésed során {.{"0h 00"}.} hosszabbítás volt a foglalásodon, <b>{{
+                        rent.vezetesi_idotartam
+                            }}</b> , ami alatt <b> {{ rent.megtett_tavolsag }} km-t</b> tettél meg, és a vezetés mellett
+                        <b> {{ rent.parkolasi_idotartam }}.</b> A bérléshez felhasznált
+                        bónusz percek:
                         0h
                         00'.
                         A bérléshez az alábbi kuponokat váltottad be:
                     </p>
 
-                    <p>A bérlésed díját, ((1,833)) Ft-ot, hamarosan kiszámlázzuk neked. A részletes díjtételeket és az
+                    <p>A bérlésed díját, <b>{{ rent.berles_osszeg }} </b> Ft-ot, hamarosan kiszámlázzuk neked. A
+                        részletes
+                        díjtételeket és az
                         autó
                         értékelése során készített képeidet tartalmazó bérlés összesítő dokumentumot <u>innen</u> tudod
                         letölteni.
@@ -51,7 +50,8 @@
                 <!-- "Keret" vége -->
 
                 <!-- 2 OPCIÓ => Töltőtt -->
-                <p class="mt-10"> Bérlésed alatt 0 kWh-t töltöttél az autóba más szolgáltató töltőoszlopán, ezért ((4,200)) Ft értékű
+                <p class="mt-10"> Bérlésed alatt 0 kWh-t töltöttél az autóba más szolgáltató töltőoszlopán, ezért
+                    ((4,200)) Ft értékű
                     PowerAndGo csomagot szereztél. A töltésért járó csomagokról bővebben <router-link
                         to="/ugyfelszolgalat" class="underline underline-offset-4 text-lime-500 ">itt</router-link>
                     olvashatsz.</p>
@@ -63,7 +63,8 @@
 
                 <p class="my-3 font-bold"> Reméljük, hogy élvezted a PowerAndGo-s utadat!</p>
 
-                <p class="mb-10"> Ha hibát észleltél vagy ötleted, észrevételed van a szolgáltatásunkkal kapcsolatban, kérjük, jelezd
+                <p class="mb-10"> Ha hibát észleltél vagy ötleted, észrevételed van a szolgáltatásunkkal kapcsolatban,
+                    kérjük, jelezd
                     nekünk az <router-link to="/ugyfelszolgalat"
                         class="underline underline-offset-4 text-lime-500 italic">ugyfelszolgalat@powerandgo.com</router-link>
                     email címen!</p>
@@ -74,44 +75,15 @@
                 <p class="my-3 font-bold italic"> Élmények. Neked. Nekünk. Tisztán.</p>
             </div>
         </div>
-
-        <!-- TÁBLÁZAT A LEKÉRT ADATOKKAL -->
-        <table class="mx-auto my-10 w-3/4 border-2 border-sky-300 border-collapse">
-            <thead>
-                <tr class="text-white text-xl bg-sky-600">
-                    <th class="px-2 ">Bérlés Azonosítója</th>
-                    <th class="px-2">Autó Rendszáma</th>
-                    <th class="px-2">Bérlés kezdete</th>
-                    <th class="px-2">Bérlés Vége</th>
-                    <th class="px-2">Bérlés Összesítője (perc)</th>
-                    <th class="px-2">Megtett táv</th>
-                    <th class="px-2">Keresztnév</th>
-                    <th class="px-2">Email cím</th>
-                </tr>
-            </thead>
-            <tbody class=" text-white text-lg">
-                <tr v-for="rent in paginatedRents" :key="rent.lezart_berles_id"
-                    class=" text-white cursor-pointer odd:hover:bg-sky-900 even:hover:bg-sky-800 odd:bg-sky-500 even:bg-sky-600">
-                    <td class="text-center p-2">{{ rent.lezart_berles_id }}</td>
-                    <td class="text-center p-2">{{ rent.auto_rendszam }}</td>
-                    <td class="text-center p-2">{{ rent.berles_kezdete }}</td>
-                    <td class="text-center p-2">{{ rent.berles_vege }}</td>
-                    <td class="text-center p-2">{{ rent.berles_percek }}</td>
-                    <td class="text-center p-2">{{ rent.megtett_tavolsag }}</td>
-                    <td class="text-center p-2">{{ rent.szemely_knev }}</td>
-                    <td class="text-center p-2">{{ rent.szemely_email }}</td>
-                </tr>
-            </tbody>
-        </table>
-
         <!-- Lapozó -->
-        <div class="pagination text-center my-5">
+        <div class="pagination text-center my-16">
             <button v-for="page in totalPages" :key="page" @click="changePage(page)"
                 :class="{ 'bg-sky-700 text-white': page === currentPage, 'bg-sky-500 text-black': page !== currentPage }"
                 class="px-3 py-2 m-1 border rounded">
                 {{ page }}
             </button>
         </div>
+
 
 
     </BaseLayout>
