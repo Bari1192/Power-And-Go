5c406b8c7e0e78cd8f5865f1f157341c99e26623
bari1192 <barabas.m4rk@gmail.com>
2024-12-16 14:37:07 +0000

    Delete és az UPDATE metódusokkal is frissült a ticket-hez tartozó tesztelési esetek.

--- fullstack_0.2/backend/tests/Feature/Http/Controllers/TicketControllerTest.php
+++ fullstack_0.2/backend/tests/Feature/Http/Controllers/TicketControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Http\Controllers;
 
+use App\Models\Ticket;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -16,7 +17,7 @@ class TicketControllerTest extends TestCase
         $response = $this->getJson('/api/cars');
         $response->assertStatus(200);
     }
-   
+
     public function test_can_create_ticket()
     {
         $ticketData = [
@@ -30,5 +31,55 @@ class TicketControllerTest extends TestCase
         $response->assertStatus(201);
 
         $this->assertDatabaseHas('tickets', ["description" => "Az autóban dohányoztak. A Dohányzás szagot és hamufoltokat hagyott az autóban. A hátsó üléseket össze is kenték."]);
+    }
+    public function test_delete_ticket(): void
+    {
+        $ticket = Ticket::create([
+            "car_id" => 10,
+            "status_id" => 6,
+            "description" => "Az autóban dohányoztak. A Dohányzás szagot és hamufoltokat hagyott az autóban. A hátsó üléseket össze is kenték."
+        ]);
+
+        $response = $this->delete("/api/tickets/{$ticket->id}");
+        $response->assertStatus(200);
+        $this->assertDatabaseMissing('tickets', ['id' => $ticket->id]);
+    }
+    
+    
+    
+    public function test_update_ticket_data(): void
+    {
+        $ticket = Ticket::create([
+            "description" => "Az autóban dohányoztak.",
+            "car_id" => 100,
+            "status_id" => 6,
+        ]);
+        $updatedData = [
+            "description" => "Az autó tiszta állapotban került vissza a flottába. Az autó 1-es státuszba került, bérelhetővé vált.",
+            "car_id" => 100,
+            "status_id" => 1,
+        ];
+        $response = $this->put("/api/tickets/{$ticket->id}", $updatedData);
+    
+        ### Ellenőrizzük a válasz státuszt
+        $response->assertStatus(200);
+    
+        ### A válaszban a frissített adatok szerepelnek?
+        $response->assertJson([
+            'data' => [
+                'id' => $ticket->id,
+                "description" => "Az autó tiszta állapotban került vissza a flottába. Az autó 1-es státuszba került, bérelhetővé vált.",
+                'car_id' => 100,
+                'status_id' => 1,
+            ]
+        ]);
+    
+        ### Az adatbázisban is, frissült a rekord?
+        $this->assertDatabaseHas('tickets', [
+            'id' => $ticket->id,
+            "description" => "Az autó tiszta állapotban került vissza a flottába. Az autó 1-es státuszba került, bérelhetővé vált.",
+            'car_id' => 100,
+            'status_id' => 1,
+        ]);
     }
 }
