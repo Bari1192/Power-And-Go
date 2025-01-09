3f38ea2d021b8db97a4dcaeca15ecf29c3a1124e
bari1192 <barabas.m4rk@gmail.com>
2025-01-09 14:55:46 +0000

    TicketController tesztjei bővítve, javítva lettek minden adat, kulcsok és kérések ellenőrzésére külön.

--- fullstack_0.2/backend/routes/api.php
+++ fullstack_0.2/backend/routes/api.php
@@ -12,8 +12,8 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('bills/filter/{type}', [BillController::class, 'filter']);
-Route::get('cars/{car}/bills', [CarController::class, 'filterCarFines']);
 
+Route::get('cars/{car}/bills', [CarController::class, 'filterCarFines']);
 Route::get('cars/{car}/description',[CarController::class, 'carLastTicketDescription']);
 Route::get('cars/{car}/tickets', [CarController::class, 'carTickets']);
 Route::get('cars/{car}/renthistory', [CarController::class, 'carWithRentHistory']);
@@ -22,14 +22,14 @@ Route::get('/bills/{car}/fees',[BillController::class,'carFees']);
 Route::get('/googlemapsapi', [GoogleMapsController::class, 'getApiUrl']);   ## Ez az input alapján a címet adja meg
 Route::get('/geocode', [GoogleMapsController::class, 'getGeocode']);        ## Ez a térképet inicializálja és jeleníti meg rajta.
 
-
+Route::apiResource('cars', CarController::class);
 Route::apiResource('tickets', TicketController::class);
+
 
 Route::apiResource('bills', BillController::class);
 
 Route::apiResource('fleets', FleetController::class);
 
-Route::apiResource('cars', CarController::class);
 Route::apiResource('carstatus', CarStatusController::class);
 
 Route::apiResource('persons', PersonController::class);
--- fullstack_0.2/backend/tests/Feature/Http/Controllers/TicketControllerTest.php
+++ fullstack_0.2/backend/tests/Feature/Http/Controllers/TicketControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Http\Controllers;
 
+use App\Models\Car;
 use App\Models\Ticket;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -9,16 +10,80 @@ use Tests\TestCase;
 
 class TicketControllerTest extends TestCase
 {
-    public function can_get_all_tickets(): void
+    public function test_can_get_all_tickets(): void
     {
-        $response = $this->getJson('/api/cars');
+        $response = $this->get('/api/tickets');
         $response->assertStatus(200);
+
+        $data = $response->json('data');
+
+        $response = $this->assertNotEmpty($data); ## ---> lekérve nem üresen jön vissza!
+    }
+    public function test_can_get_ticket_id(): void
+    {
+        $ticket = Ticket::FirstOrFail();
+
+        $response = $this->get("/api/tickets/{$ticket->id}");
+        $response->assertStatus(200);
+
+        $data = $response->json('data');
+        $response = $this->assertArrayHasKey('id', $data, 'az `id` nem töltődött be hozzá.');
+    }
+    public function test_can_get_ticket_description(): void
+    {
+        $ticket = Ticket::FirstOrFail();
+        $response = $this->get("/api/tickets/{$ticket->id}");
+        $response->assertStatus(200);
+
+        $data = $response->json('data');
+        $response = $this->assertArrayHasKey('description', $data, 'a `description` nem töltődött be hozzá.');
+    }
+    public function test_can_get_ticket_with_car_id(): void
+    {
+        $ticket = Ticket::FirstOrFail();
+        $response = $this->get("/api/tickets/{$ticket->id}");
+
+        $response->assertStatus(200);
+
+        $data = $response->json('data');
+        $response = $this->assertArrayHasKey('car_id', $data, 'a `car_id` nem töltődött be hozzá.');
+    }
+    public function test_can_get_ticket_with_car_status_id(): void
+    {
+        $ticket = Ticket::FirstOrFail();
+        $response = $this->get("/api/tickets/{$ticket->id}");
+
+        $response->assertStatus(200);
+
+        $data = $response->json('data');
+        $response = $this->assertArrayHasKey('status_id', $data, 'a `status_id` nem töltődött be hozzá.');
+    }
+    public function test_can_get_ticket_with_car_status_status_description(): void
+    {
+        $ticket = Ticket::FirstOrFail();
+        $response = $this->get("/api/tickets/{$ticket->id}");
+
+        $response->assertStatus(200);
+
+        $data = $response->json('data');
+        $response = $this->assertArrayHasKey('status_descrip', $data, 'a `status_descrip`rész nem töltődött be hozzá.');
+    }
+    public function test_can_get_ticket_with_report_date_time(): void
+    {
+        $ticket = Ticket::FirstOrFail();
+        $response = $this->get("/api/tickets/{$ticket->id}");
+
+        $response->assertStatus(200);
+
+        $data = $response->json('data');
+        $response = $this->assertArrayHasKey('bejelentve', $data, 'a `bejelentve`rész nem töltődött be hozzá.');
     }
 
-    public function can_create_ticket()
+    public function test_can_create_ticket_to_the_first_car()
     {
+        $car = Car::FirstOrFail();
         $ticketData = [
-            "car_id" => 10,
+            "car_id" => $car->id,
             "status_id" => 6,
             "description" => "Az autóban dohányoztak. A Dohányzás szagot és hamufoltokat hagyott az autóban. A hátsó üléseket össze is kenték."
         ];
@@ -29,10 +94,12 @@ class TicketControllerTest extends TestCase
 
         $this->assertDatabaseHas('tickets', ["description" => "Az autóban dohányoztak. A Dohányzás szagot és hamufoltokat hagyott az autóban. A hátsó üléseket össze is kenték."]);
     }
-    public function delete_ticket(): void
+    public function test_can_delete_ticket_from_the_first_car_by_last_ticket_id(): void
     {
+
+        $car = Car::FirstOrFail();
         $ticket = Ticket::create([
-            "car_id" => 10,
+            "car_id" => $car->id,
             "status_id" => 6,
             "description" => "Az autóban dohányoztak. A Dohányzás szagot és hamufoltokat hagyott az autóban. A hátsó üléseket össze is kenték."
         ]);
@@ -41,42 +108,26 @@ class TicketControllerTest extends TestCase
         $response->assertStatus(200);
         $this->assertDatabaseMissing('tickets', ['id' => $ticket->id]);
     }
-    
-    
-    
-    public function update_ticket_data(): void
+
+    public function test_can_update_first_ticket_status_id_two_times(): void
     {
-        $ticket = Ticket::create([
-            "description" => "Az autóban dohányoztak.",
-            "car_id" => 100,
-            "status_id" => 6,
-        ]);
-        $updatedData = [
-            "description" => "Az autó tiszta állapotban került vissza a flottába. Az autó 1-es státuszba került, bérelhetővé vált.",
-            "car_id" => 100,
-            "status_id" => 1,
+        $ticket = Ticket::FirstOrFail();
+        $car=Car::FirstOrFail();
+        $ticketData = [
+            "car_id" => $car->id,
+            "status_id" => 7,
+            "description" => "Az autóban dohányoztak. A Dohányzás szagot és hamufoltokat hagyott az autóban. A hátsó üléseket össze is kenték."
         ];
-        $response = $this->put("/api/tickets/{$ticket->id}", $updatedData);
-    
-        ### Ellenőrizzük a válasz státuszt
+
+        $response = $this->put("/api/tickets/{$ticket->id}", $ticketData);
         $response->assertStatus(200);
-    
-        ### A válaszban a frissített adatok szerepelnek?
-        $response->assertJson([
-            'data' => [
-                'id' => $ticket->id,
-                "description" => "Az autó tiszta állapotban került vissza a flottába. Az autó 1-es státuszba került, bérelhetővé vált.",
-                'car_id' => 100,
-                'status_id' => 1,
-            ]
-        ]);
-    
-        ### Az adatbázisban is, frissült a rekord?
-        $this->assertDatabaseHas('tickets', [
-            'id' => $ticket->id,
-            "description" => "Az autó tiszta állapotban került vissza a flottába. Az autó 1-es státuszba került, bérelhetővé vált.",
-            'car_id' => 100,
-            'status_id' => 1,
-        ]);
+
+        $secondTicketData=[
+            "car_id" => $car->id,
+            "status_id" => 1,
+            "description" => "Az autó tisztítása megtörtént, újra forgalomba, helyezem. Foglalhatóvá vált."
+        ];
+        $response = $this->put("/api/tickets/{$ticket->id}", $secondTicketData);
+        $response->assertStatus(200);
     }
 }
