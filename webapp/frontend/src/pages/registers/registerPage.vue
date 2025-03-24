d10f5e082a678f2ae5fb261cab4d3e15f92a230c
bari1192 <barabas.m4rk@gmail.com>
2025-03-24 12:55:02 +0000

    stilisztikai javítás

--- webapp/frontend/src/pages/registers/registerPage.vue
+++ webapp/frontend/src/pages/registers/registerPage.vue
@@ -1,7 +1,11 @@
 <template>
     <BaseLayout>
-        <div class="min-h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8 mb-24">
-            <div id="headerbg" class="max-w-4xl w-full space-y-8  p-8 rounded-2xl shadow-2xl">
+        <div class="h-10/12 flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8 mb-24"
+            style="background: linear-gradient(to bottom, rgba(255, 255, 255, 0.50), 
+            rgba(255, 255, 255, .40),
+            rgba(255, 255, 255, .01));">
+
+            <div id="headerbg" class="max-w-4xl w-full space-y-8  p-8 rounded-2xl shadow-2xl border-x-8 border-lime-900 border-opacity-85">
                 <!-- Fejléc -->
                 <div id="companyname" class="text-center">
                     <h2 class="text-4xl pt-5 pr-4 italic text-right font-extrabold text-white tracking-wider">
@@ -10,10 +14,10 @@
                 </div>
 
                 <!-- Léptető -->
-                <div class="flex justify-center space-x-4">
+                <div class="flex justify-center space-x-6">
                     <div v-for="(step, index) in steps" :key="index" class="flex flex-col items-center">
                         <div :class="[
-                            'w-12 h-12 rounded-full flex items-center justify-center mb-2 transition-all duration-1000',
+                            'w-10 h-10 rounded-full flex items-center justify-center mb-2 transition-all duration-1000',
                             currentStep === index + 1
                                 ? 'bg-amber-100 text-lime-900 ring-4 ring-yellow-400 font-semibold text-xl'
                                 : 'bg-lime-800 text-lime-100 ring-2 ring-lime-400/30'
@@ -24,8 +28,8 @@
                     </div>
                 </div>
 
-                <p class="text-white text-4xl ml-2 tracking-wide font-semibold">Regisztráció</p>
                 <div v-if="currentStep === 1" class="space-y-6 transition-all  duration-1000">
+                    <p class="text-white text-3xl ml-2 tracking-wide font-semibold">Regisztráció</p>
                     <div
                         class="bg-gradient-to-r from-lime-800/40 to-transparent p-4 rounded-lg border-4 border-yellow-200 border-opacity-75 backdrop-blur-sm">
                         <h3 class="text-2xl font-bold text-lime-100 mb-6 border-l-8 border-yellow-400 pl-4">
@@ -33,20 +37,18 @@
                         </h3>
                         <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
                             <FormKit type="text" name="lastname" v-model="formData.lastname" label="Vezetéknév"
-                                placeholder="Adja meg vezetéknevét" validation="required"
-                                :validation-messages="{ required: 'Kötelező mező' }" :classes="formClasses" />
-
-                            <FormKit type="text" name="firstname" v-model="formData.firstname" label="Keresztnév"
-                                placeholder="Adja meg keresztnevét" validation="required"
-                                :validation-messages="{ required: 'Kötelező mező' }" :classes="formClasses" />
-
-                            <FormKit type="date" name="birth_date" v-model="formData.birth_date" label="Születési dátum"
-                                validation="required" :validation-messages="{ required: 'Kötelező mező' }"
+                                placeholder="Adja meg vezetéknevét" validation="" :validation-messages="{}"
                                 :classes="formClasses" />
 
+                            <FormKit type="text" name="firstname" v-model="formData.firstname" label="Keresztnév"
+                                placeholder="Adja meg keresztnevét" validation="" :validation-messages="{}"
+                                :classes="formClasses" />
+
+                            <FormKit type="date" name="birth_date" v-model="formData.birth_date" label="Születési dátum"
+                                validation="" :validation-messages="{}" :classes="formClasses" />
+
                             <FormKit type="tel" name="phone" v-model="formData.phone" label="Telefonszám"
-                                placeholder="+36301234567" validation="required" :validation-messages="{
-                                    required: 'Kötelező mező',
+                                placeholder="+36301234567" validation="" :validation-messages="{
                                     matches: 'Érvénytelen telefonszám formátum'
                                 }" :classes="formClasses" />
                         </div>
@@ -59,7 +61,7 @@
                     </div>
                 </div>
 
-                <!-- Második lépés: Dokumentum adatok -->
+                <!-- Második lépés-->
                 <div v-if="currentStep === 2" class="space-y-6 transition-all duration-1000">
                     <div class="bg-gradient-to-r from-lime-800/40 to-transparent p-4 rounded-lg backdrop-blur-sm">
                         <h3 class="text-2xl font-bold text-lime-100 mb-6 border-l-4 border-yellow-400 pl-4">
@@ -67,30 +69,27 @@
                         </h3>
                         <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
                             <FormKit type="text" name="id_card" v-model="formData.id_card"
-                                label="Személyi igazolvány szám" placeholder="123456AA" validation="required"
+                                label="Személyi igazolvány szám" placeholder="123456AA" validation=""
                                 :validation-messages="{ required: 'Kötelező mező' }" :classes="formClasses" />
 
                             <FormKit type="text" name="driving_license" v-model="formData.driving_license"
-                                label="Jogosítvány szám" placeholder="AB123456" validation="required"
+                                label="Jogosítvány szám" placeholder="AB123456" validation=""
                                 :validation-messages="{ required: 'Kötelező mező' }" :classes="formClasses" />
 
                             <FormKit type="date" name="license_start_date" v-model="formData.license_start_date"
-                                label="Jogosítvány kiállítási dátuma" validation="required"
+                                label="Jogosítvány kiállítási dátuma" validation=""
                                 :validation-messages="{ required: 'Kötelező mező' }" :classes="formClasses" />
 
                             <FormKit type="date" name="license_end_date" v-model="formData.license_end_date"
-                                label="Jogosítvány érvényességi ideje" validation="required"
+                                label="Jogosítvány érvényességi ideje" validation=""
                                 :validation-messages="{ required: 'Kötelező mező' }" :classes="formClasses" />
 
                         </div>
                         <div class="flex justify-center space-x-4 pt-6">
-                            <!-- Vissza gomb - csak ha nem az első lépésnél vagyunk -->
                             <button v-if="currentStep > 1" @click.prevent="prevStep"
                                 class="px-8 py-3 bg-lime-800 text-lime-100 rounded-lg hover:bg-lime-900 transform hover:-translate-y-1 transition-all duration-200 shadow-lg hover:shadow-lime-900/50">
                                 Vissza
                             </button>
-
-                            <!-- Következő gomb - csak ha nem az utolsó lépésnél vagyunk -->
                             <button v-if="currentStep < 3" @click.prevent="nextStep"
                                 class="px-8 py-3 bg-yellow-500 text-lime-900 rounded-lg hover:bg-yellow-600 transform hover:-translate-y-1 transition-all duration-200 shadow-lg hover:shadow-yellow-500/50">
                                 Következő
@@ -99,7 +98,7 @@
                     </div>
                 </div>
 
-                <!-- Harmadik lépés: Felhasználói fiók adatok -->
+                <!-- Harmadik lépés-->
                 <div v-if="currentStep === 3" class="space-y-6 transition-all duration-1000">
                     <div class="bg-gradient-to-r from-lime-800/40 to-transparent p-4 rounded-lg backdrop-blur-sm">
                         <h3 class="text-2xl font-bold text-lime-100 mb-6 border-l-4 border-yellow-400 pl-4">
@@ -107,33 +106,56 @@
                         </h3>
                         <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
                             <FormKit type="text" name="user_name" v-model="formData.user_name" label="Felhasználónév"
-                                placeholder="felhasználónév" validation="required"
+                                placeholder="felhasználónév" validation=""
                                 :validation-messages="{ required: 'Kötelező mező' }" :classes="formClasses" />
 
                             <FormKit type="password" name="person_password" v-model="formData.person_password"
-                                label="Jelszó" validation="required|length:8" :validation-messages="{
+                                label="Jelszó" validation="" :validation-messages="{
                                     required: 'Kötelező mező',
                                     length: 'A jelszónak minimum 8 karakterből kell állnia'
                                 }" :classes="formClasses" />
 
                             <FormKit type="text" name="pin" v-model="formData.pin" label="PIN kód" placeholder="1234"
-                                validation="required|matches:/^[0-9]{4}$/" :validation-messages="{
+                                validation="" :validation-messages="{
                                     required: 'Kötelező mező',
                                     matches: 'A PIN kódnak 4 számjegyből kell állnia'
                                 }" :classes="formClasses" />
-                            <div class="flex justify-center space-x-4 pt-6">
-                                <!-- VISSZA -->
-                                <button v-if="currentStep > 1" @click.prevent="prevStep"
-                                    class="px-8 py-3 bg-lime-800 text-lime-100 rounded-lg hover:bg-lime-900 transform hover:-translate-y-1 transition-all duration-200 shadow-lg hover:shadow-lime-900/50">
-                                    Vissza
-                                </button>
-                                <!-- Véglegesítés -->
-                                <button v-if="currentStep === 3" type="submit" @click="submitRegistration"
-                                    class="px-8 py-3 bg-yellow-500 text-lime-900 rounded-lg hover:bg-yellow-600 transform hover:-translate-y-1 transition-all duration-200 shadow-lg hover:shadow-yellow-500/50">
-                                    Regisztráció véglegesítése
-                                </button>
+
+                            <div class="inline-flex items-center">
+                                <label class="flex items-center cursor-pointer relative">
+                                    <input type="checkbox"
+                                        class="peer h-6 w-6 border-2 cursor-pointer transition-all appearance-none rounded shadow hover:shadow-md border-yellow-100 checked:bg-green-600 checked:border-green-600"
+                                        id="check4" />
+                                    <span
+                                        class="absolute text-white opacity-0 peer-checked:opacity-100 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 20 20"
+                                            fill="currentColor" stroke="currentColor" stroke-width="1">
+                                            <path fill-rule="evenodd"
+                                                d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                                                clip-rule="evenodd"></path>
+                                        </svg>
+                                    </span>
+                                </label>
+                                <span for="link-checkbox" class="pl-4 text-sm font-medium text-lime-100">Regisztrációm
+                                    elküldésével hozzájárulok és megerősítem a személyes adataim kezelését az
+                                    <a href="#" class="text-lime-300 italic underline">általános szerződési
+                                        feltételekben</a> foglaltak alapján.</span>
                             </div>
                         </div>
+                    </div>
+
+
+                    <div class="flex justify-center space-x-4 pt-6">
+                        <!-- VISSZA -->
+                        <button v-if="currentStep > 1" @click.prevent="prevStep"
+                            class="px-8 py-3 bg-lime-800 text-lime-100 rounded-lg hover:bg-lime-900 transform hover:-translate-y-1 transition-all duration-200 shadow-lg hover:shadow-lime-900/50">
+                            Vissza
+                        </button>
+                        <!-- Véglegesítés -->
+                        <button v-if="currentStep === 3" type="submit" @click="submitRegistration"
+                            class="px-8 py-3 bg-yellow-500 text-lime-900 rounded-lg hover:bg-yellow-600 transform hover:-translate-y-1 transition-all duration-200 shadow-lg hover:shadow-yellow-500/50">
+                            Regisztráció véglegesítése
+                        </button>
                     </div>
                 </div>
             </div>
@@ -142,6 +164,12 @@
 </template>
 
 <style scoped>
+#bglayer {
+    background: linear-gradient(rgba(255, 255, 255, 0.5),
+            rgba(255, 255, 255, 0.5),
+            rgba(101, 163, 13, .3)),
+}
+
 #headerbg {
     background: linear-gradient(to bottom,
             rgba(84, 139, 10, .9),
@@ -164,7 +192,8 @@
     background: url('@img/BaseEmail/largebanner.png');
     background-size: cover;
     background-position: center;
-    height: 90px;
+    min-height: 90px;
+
 }
 
 button {
@@ -224,7 +253,7 @@ import { ref, reactive } from 'vue';
 import { http } from '@/utils/http.mjs';
 import { toast } from 'vue3-toastify';
 
-const steps = ['Személyes adatok', 'Felhasználói profil', 'Összesítő'];
+const steps = ['Személyes adatok', 'Felhasználói profil', 'Véglegesítés'];
 const currentStep = ref(1);
 
 const formData = reactive({
